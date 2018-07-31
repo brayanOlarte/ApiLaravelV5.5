6436ed46b29dd3fcbc87309e33a91042419ccfe3
brayan <brayanolarte972@gmail.com>
2018-07-31 17:06:33 -0500

    creación del formulario

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Formulario</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -62,34 +62,75 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
+            .thumb {
+           height: 300px;
+           border: 1px solid #000;
+           margin: 10px 5px 0 0;
+         }
+         body{height:100%;
+              width:100%;
+              background-image:url(12.jpg);
+              background-repeat:no-repeat;
+              background-size:cover;
+            }
         </style>
     </head>
-    <body>
+    <body background="img/12.jpg">
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
+
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Formulario
                 </div>
 
+                    <input type="file" id="files" name="files[]" />
+                    <br />
+                    <output id="list"></output>
+
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    Nombre:<br>
+                    <input type="text" name="firstname"><br>
+                    Apellido:<br>
+                    <input type="text" name="lastname"><br>
+                    Usuario:<br>
+                    <input type="text" name="firstname"><br>
+                    Contraseña:<br>
+                    <input type="password" name="lastname"><br>
+                    Email:<br>
+                    <input type="email" name="email"><br>
+                    <input type="submit" value="Enviar">
+                    <input type="submit" value="Eliminar">
+                    <input type="submit" value="Buscar">
+                    <input type="submit" value="Modificar">
                 </div>
             </div>
-        </div>
+
+        <script>
+              function archivo(evt) {
+                  var files = evt.target.files; // FileList object
+
+                  // Obtenemos la imagen del campo "file".
+                  for (var i = 0, f; f = files[i]; i++) {
+                    //Solo admitimos imágenes.
+                    if (!f.type.match('image.*')) {
+                        continue;
+                    }
+
+                    var reader = new FileReader();
+
+                    reader.onload = (function(theFile) {
+                        return function(e) {
+                          // Insertamos la imagen
+                         document.getElementById("list").innerHTML = ['<img class="thumb" src="', e.target.result,'" title="', escape(theFile.name), '"/>'].join('');
+                        };
+                    })(f);
+
+                    reader.readAsDataURL(f);
+                  }
+              }
+
+              document.getElementById('files').addEventListener('change', archivo, false);
+      </script>
     </body>
 </html>
