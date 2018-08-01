49869fb3675649927f90eb29b1a1ec3a5a99ccd8
brayan <brayanolarte972@gmail.com>
2018-07-31 19:33:08 -0500

    envio de datos

--- routes/api.php
+++ routes/api.php
@@ -13,11 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-/*Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-});*/
-
-Route::apiResource('users', 'UserController');
-Route::apiResource('products', 'ProductController');
-Route::apiResource('transactions', 'TransactionController');
-Route::apiResource('categories', 'CategoryController');
+});
